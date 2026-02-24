1bd61d579b37d87bc8ed32869bce7a139043c602
Tran Hoang Giang <tranhoanggiang5@gmail.com>
2026-02-24 19:45:24 +0700

    Initial Commit

---
+++ Order Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Order Manager
+//
+//  Created by Vũ Thị Hà Giang on 24/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Order Manager/Order_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Order_ManagerApp.swift
+//  Order Manager
+//
+//  Created by Vũ Thị Hà Giang on 24/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Order_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
